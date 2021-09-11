cb00f6640ad44f387833e16f559b15b797ccd9be
aman-luthra1 <luthraaman301@gmail.com>
2021-09-11 14:08:16 +0530

    Complete Blog-Post

--- main.py
+++ main.py
@@ -13,14 +13,16 @@ from flask import abort
 from sqlalchemy import Table, Column, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-import os
+import config
 
 Base = declarative_base()
 
 # gunicorn version 20.1.0
 # psycopg2-binary version 2.9.1
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+# app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = config.DATABASE_URL
+
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
@@ -39,9 +41,9 @@ gravatar = Gravatar(app,
 
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'DATABASE_URL'
+app.config['SQLALCHEMY_DATABASE_URI'] = config.DATABASE_URL
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///blog_1.db')
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///blog_1.db')
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog_1.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
